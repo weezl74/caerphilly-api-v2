86fa1280412e80e2b7730d78df38ab165a3633d7
weezl74 <louise.woolley1@googlemail.com>
2026-07-06 12:33:07 +0100

    Fix SQL database configuration variable name

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const config = {
   user: process.env.SQL_USER,
   password: process.env.SQL_PASSWORD,
   server: process.env.SQL_SERVER,
-  database: process.env.SQL_DB,
+ database: process.env.SQL_DATABASE,
   options: {
     encrypt: true
   }
